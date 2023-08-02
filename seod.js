9f55fcc9c82c1f3d8b22bd001edbfe75f462a606
kizkulesi <85099543+kizkulesi@users.noreply.github.com>
2023-08-02 10:53:02 +0300

    Create seod.js

---
+++ seod.js
@@ -0,0 +1,12 @@
+<script type="text/javascript"> 
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('//movesickly.com/csb2xqyn?key=8761492a39155a935456bc2f07498ed3', '_blank');
+	});
+</script>
