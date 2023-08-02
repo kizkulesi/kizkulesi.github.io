b4bf27a5f5fb645d6beabc239e609a544a1268d1
kizkulesi <85099543+kizkulesi@users.noreply.github.com>
2023-08-02 10:40:21 +0300

    Create seo300.js

---
+++ seo300.js
@@ -0,0 +1,11 @@
+<center>
+<script type="text/javascript">
+atOptions = {
+'key' : '7cb156303975edbb8a8e44c369121e79',
+'format' : 'iframe',
+'height' : 250,
+'width' : 300,
+'params' : {}
+};
+</script>
+<script type="text/javascript" src="//movesickly.com/7cb156303975edbb8a8e44c369121e79/invoke.js"></script></center>
